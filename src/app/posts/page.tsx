ceab37dee4a2e16bc6775d06c2ae47a3599012ad
abhinavgkrishnan <abhinavgkrishnan@gmail.com>
2024-12-11 21:47:04 +0530

    responsive posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,7 +3,7 @@ import { posts } from "@/data/posts"
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+    <div className="flex flex-1 flex-col gap-4 p-2 sm:p-4 pt-0">
       <div className="flex flex-col gap-4">
         {posts.map((post) => (
           <PostCard key={post.id} post={post} />
